f7c2bd2afb2c873d63eec5ad2cf5dfa0afc0122e
Björn Kihlberg <bjorn.wilhelm.kihlberg@gmail.com>
2022-08-07 16:48:59 +0200

    basic pattern matching

---
+++ match.scm
@@ -0,0 +1,45 @@
+(library (match)
+  (export if-match match)
+  (import (chezscheme))
+  (define-syntax if-match
+    (syntax-rules (unquote-splicing quasiquote unquote)
+      [(_ value `,x bodys bodyf) ((lambda (x) bodys) value)]
+      [(_ value `,x t bodys bodyf) ((lambda (x) (if t bodys bodyf)) value)]
+      [(_ value `(,@x) bodys bodyf)
+       (if (list? value) ((lambda (x) bodys) value) bodyf)]
+      [(_ value `(,@x) t bodys bodyf)
+       (if (list? value) ((lambda (x) (if t bodys bodyf)) value) bodyf)]
+      [(_ value `(x) bodys bodyf)
+       (if (and (pair? value) (null? (cdr value)))
+           (if-match (car value) `x bodys bodyf)
+           bodyf)]
+      [(_ value `(x) t bodys bodyf)
+       (if (and (pair? value) (null? (cdr value)))
+           (if-match (car value) `x t bodys bodyf)
+           bodyf)]
+      [(_ value `(x xs ...) bodys bodyf)
+       (if (and (pair? value) (not (null? (cdr value))))
+           (if-match
+             (car value)
+             `x
+             (if-match (cdr value) `(xs ...) bodys bodyf)
+             bodyf)
+           bodyf)]
+      [(_ value `(x xs ...) t bodys bodyf)
+       (if (and (pair? value) (not (null? (cdr value))))
+           (if-match
+             (car value)
+             `x
+             (if-match (cdr value) `(xs ...) t bodys bodyf)
+             bodyf)
+           bodyf)]
+      [(_ value `x bodys bodyf) (if (equal? value 'x) bodys bodyf)]
+      [(_ value `x t bodys bodyf) (if (and (equal? value 'x) t) bodys bodyf)]))
+  (define-syntax match
+    (syntax-rules (else)
+      [(match value) (void)]
+      [(match value [else body]) body]
+      [(match value [pattern body] rest ...)
+       (if-match value pattern body (match value rest ...))]
+      [(match value [pattern test body] rest ...)
+       (if-match value pattern test body (match value rest ...))])))
